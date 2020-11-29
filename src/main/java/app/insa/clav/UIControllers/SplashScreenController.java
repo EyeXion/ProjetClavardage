5544a87131a39d9f42b853c6b9b0d3c0cd46172e
EyeXion <elies.tali18@gmail.com>
2020-11-29 21:35:56 +0100

    Added cool animation ssplash screen

--- src/main/java/app/insa/clav/UIControllers/SplashScreenController.java
+++ src/main/java/app/insa/clav/UIControllers/SplashScreenController.java
@@ -1,12 +1,14 @@
 package app.insa.clav.UIControllers;
 
 import javafx.animation.FadeTransition;
+import javafx.animation.RotateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -19,12 +21,27 @@ public class SplashScreenController implements Initializable {
     @FXML
     private StackPane rootSplash;
 
+    @FXML
+    private Circle c1;
+
+    @FXML
+    private Circle c2;
+
+    @FXML
+    private Circle c3;
+
 
     public SplashScreenController(){}
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        this.setRotateCircles(c1,true,360,10);
+        this.setRotateCircles(c2,true,180,18);
+        this.setRotateCircles(c3,true,345,24);
+
+
         FadeTransition fadeIn = new FadeTransition(Duration.seconds(2),rootSplash);
         fadeIn.setFromValue(0);
         fadeIn.setToValue(1);
@@ -54,5 +71,16 @@ public class SplashScreenController implements Initializable {
             }
         });
 
+    }
+
+    private void setRotateCircles(Circle c, boolean reverse, int angle, int time){
+        RotateTransition rot = new RotateTransition(Duration.seconds(time),c);
+
+        rot.setByAngle(angle);
+        rot.setAutoReverse(reverse);
+        rot.setDelay(Duration.ZERO);
+        rot.setRate(6);
+        rot.setCycleCount(3);
+        rot.play();
     }
 }
